8d3f7f398b0ce876b122e9cef37c41252ecb68c1
LABARBE Léo <labarbe.leo2308@gmail.com>
2025-03-06 19:59:29 +0100

    Vue des pétitions V1.0

--- Sources/view_petition.php
+++ Sources/view_petition.php
@@ -9,18 +9,23 @@ include_once 'header.php';
     <p>Category</p>
 </div>
 
-<div class="petition_description">
-    <h2>Description</h2>
+<div class="petition_content">
+    <div class="petition_description">
+        <h2>Description de la Pétition :</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Saepe corporis ducimus, aliquid doloremque nesciunt quasi sunt dolorem veniam eum ipsam libero. Inventore voluptas error rem architecto quisquam unde sequi hic! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam qui hic sapiente perferendis, in quasi consectetur, ex est sint reiciendis necessitatibus natus atque impedit neque cum molestias quos beatae eum. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Et, aliquam odio. Mollitia quae cum iste dicta in cumque facere sint labore nostrum nesciunt odio, delectus ea, laudantium possimus, fugit architecto.</p>
+    </div>
     <hr>
-    <p>Texte de la pétition</p>
-</div>
-<div class="petition_signatures">
-    <h2>Signatures</h2>
-    <p>Nombre de signatures</p>
-</div>
-<div class="petition_creator">
-    <h2>Créateur</h2>
-    <p>Nom du créateur</p>
+    <div class="bottom_grid">
+        <div class="petition_signatures">
+            <h2>Signatures :</h2>
+            <p>XXX / XXX</p>
+        </div>
+        <div class="spacing">&nbsp;|&nbsp;</div>
+        <div class="petition_creator">
+            <h2>Auteur :</h2>
+            <p>Nom du créateur</p>
+        </div>
+    </div>
 </div>
 
 <?php
