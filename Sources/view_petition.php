bf71ae413db35735da8f11e9416cf4056f38e410
LABARBE Léo <labarbe.leo2308@gmail.com>
2025-03-06 19:30:55 +0100

    Small changes

---
+++ Sources/view_petition.php
@@ -0,0 +1,28 @@
+<?php
+include_once 'header.php';
+?>
+
+<link rel="stylesheet" href="css/view_petition.css">
+
+<div class="petition_header">
+    <h1 class="petition_name">Nom de la Pétition</h1>
+    <p>Category</p>
+</div>
+
+<div class="petition_description">
+    <h2>Description</h2>
+    <hr>
+    <p>Texte de la pétition</p>
+</div>
+<div class="petition_signatures">
+    <h2>Signatures</h2>
+    <p>Nombre de signatures</p>
+</div>
+<div class="petition_creator">
+    <h2>Créateur</h2>
+    <p>Nom du créateur</p>
+</div>
+
+<?php
+include_once 'footer.php';
+?>
