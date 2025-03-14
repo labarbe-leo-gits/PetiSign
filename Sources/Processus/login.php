fbf841e8d63f6120f81b9693990d60bc07c38a07
LABARBE Léo <labarbe.leo2308@gmail.com>
2025-03-14 21:54:04 +0100

    Start of login/logout logic

--- Sources/Processus/login.php
+++ Sources/Processus/login.php
@@ -1,3 +1,25 @@
 <?php
 
+include_once '../database/database.php';
+session_start();
+
+$username = $_POST['mail'];
+$password = $_POST['password'];
+
+$stmt = $pdo->prepare("SELECT password FROM USER WHERE email = :mail");
+$stmt->bindParam(':mail', $username);
+$stmt->execute();
+$hashedPassword = $stmt->fetchColumn();
+
+if ($hashedPassword && password_verify($password, $hashedPassword)) {
+    // retrieves username from database and echo hello [user]
+    $stmt = $pdo->prepare("SELECT username FROM USER WHERE email = :mail");
+    $stmt->bindParam(':mail', $username);
+    $stmt->execute();
+    $user = $stmt->fetchColumn();
+    echo "Hello " . $user;
+    $_SESSION['mail'] = $username;
+} else {
+    echo "Wrong password or user not found";
+}
 ?>
---
+++ Sources/logout.php
@@ -0,0 +1,10 @@
+<?php
+
+function logout() {
+    session_start();
+    session_destroy();
+    header('Location: login.php');
+    exit();
+}
+
+?>
